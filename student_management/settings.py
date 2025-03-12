00894c68877b68aa6f13c12db53f6ebd0ce10531
Nahid Khandaker <96949937+nahidkhandaker@users.noreply.github.com>
2025-03-13 03:00:37 +0600

    Update host

--- student_management/settings.py
+++ student_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fnv$25i5=i&)7x1i@3fg8$wv15ci$2=vv_6wo+__79b!n@r3_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['student-management-system-kezf.onrender.com']
 
 # Message tags for Boostrap
 
